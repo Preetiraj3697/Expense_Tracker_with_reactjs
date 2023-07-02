3c4d0c84c2a8ef33a3690b009afe35f2d3a873d7
Preeti Raj <satendra11kum@gmail.com>
2023-07-02 22:28:13 +0530

    Initial commit

---
+++ src/Components/NewExpense/NewExpense.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ExpenseFrom from "./ExpenseFrom";
+import "./NewExpense.css";
+function NewExpense() {
+  return (
+    <div className="new-expense">
+      <ExpenseFrom />
+    </div>
+  );
+}
+export default NewExpense;
